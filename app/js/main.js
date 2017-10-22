27cecbf90d61939979f9626cef1658cf25e761bc
Joey Zhang <xiaochi1214@hotmail.com>
2017-10-22 16:07:46 -0500

    updates - added new page

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,28 @@
-$("#open-content").click(function() {
-  var iframe = $("#project-1");
-  iframe.attr("src", iframe.data("src"));
-  var elem = document.getElementById('project1');
-  elem.classList.remove("is-hidden");
-});
+// $("#open-content").click(function() {
+//   var iframe = $("#project-1");
+//   iframe.attr("src", iframe.data("src"));
+//   var elem = document.getElementById('project1');
+//   elem.classList.remove("is-hidden");
+// });
+//
+// $("#close-content").click(function() {
+//   var elem = document.getElementById('project1');
+//   elem.classList.add("is-hidden");
+// });
 
-$("#close-content").click(function() {
-  var elem = document.getElementById('project1');
-  elem.classList.add("is-hidden");
-});
+var links = ['https://www.facebook.com/', 'https://twitter.com', 'https://soundcloud.com'];
+
+
+
+
+window.addEventListener('load', function() {
+  var listElement = document.getElementsByClassName('nav-link');
+  for (i = 0; i < listElement.length; i++) {
+    listElement[i].addEventListener('click', openURL.bind(null, event, links[i]))
+  }
+})
+
+
 
 $("#open-contact").click(function() {
   var elem = document.getElementById('contact');
