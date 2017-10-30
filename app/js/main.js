ebc050a78f02306d3c3f900bc0af20dabc51b1d8
Joey Zhang <xiaochi1214@hotmail.com>
2017-10-29 19:20:44 -0500

    updates

--- app/js/main.js
+++ app/js/main.js
@@ -9,27 +9,37 @@
 //   var elem = document.getElementById('project1');
 //   elem.classList.add("is-hidden");
 // });
-
-var links = ['https://www.facebook.com/', 'https://twitter.com', 'https://soundcloud.com'];
-
-
-
-
-window.addEventListener('load', function() {
-  var listElement = document.getElementsByClassName('nav-link');
-  for (i = 0; i < listElement.length; i++) {
-    listElement[i].addEventListener('click', openURL.bind(null, event, links[i]))
-  }
-})
-
-
-
+//
+// var links = ['https://www.facebook.com/', 'https://twitter.com', 'https://soundcloud.com'];
+//
+//
+//
+//
+// window.addEventListener('load', function() {
+//   var listElement = document.getElementsByClassName('nav-link');
+//   for (i = 0; i < listElement.length; i++) {
+//     listElement[i].addEventListener('click', openURL.bind(null, event, links[i]))
+//   }
+// })
+//
+//
+//
 $("#open-contact").click(function() {
   var elem = document.getElementById('contact');
-  elem.classList.remove("is-hidden");
+  elem.classList.remove("invisible");
 });
 
 $("#open-contact").siblings().click(function() {
   var elem = document.getElementById('contact');
-  elem.classList.add("is-hidden");
+  elem.classList.add("invisible");
 });
+
+
+var hideButton = document.getElementsByClassName("js-hideButton");
+for (var i = 0; i < hideButton.length; i++) {
+	hideButton[i].addEventListener("click", function(){
+		[].forEach.call(document.querySelectorAll("li.show"), function(hideContent){
+			hideContent.classList.remove("show");
+		});
+	});
+}
