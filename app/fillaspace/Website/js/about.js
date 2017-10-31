4e9b19bf5fd1e50722522b4007021a818e054586
Joey Zhang <xiaochi1214@hotmail.com>
2017-10-30 20:41:17 -0500

    added fillaspace website

---
+++ app/fillaspace/Website/js/about.js
@@ -0,0 +1,11 @@
+(function(){
+      var words = [
+          'Chongchi',
+          'Joey'
+          ], i = 0;
+      setInterval(function(){
+          $('#changingword').fadeOut(function(){
+              $(this).html(words[i=(i+1)%words.length]).fadeIn();
+          });
+      }, 3000);
+})();
